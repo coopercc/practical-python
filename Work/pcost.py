48fa2639934ceec16dc1bc68977abc4c72e3240f
Cooper Cain <cooper@coopercc.me>
2025-03-11 09:12:48 -0700

    Lesson 1.6

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,13 @@
 # pcost.py
 #
 # Exercise 1.27
+
+total_cost = 0
+
+with open("Data/portfolio.csv", "rt") as f:
+    headers = next(f)
+    for line in f:
+        data = line.split(",")
+        total_cost = total_cost + (int(data[1]) * float(data[2]))
+
+print("Total Cost", total_cost)
